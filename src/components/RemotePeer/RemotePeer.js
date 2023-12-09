3fd1c647b96d64ea100b6a902044ad488b4d8013
ipanditi <vivekpanditdv@gmail.com>
2023-12-10 03:32:13 +0530

    hardcoding api key

---
+++ src/components/RemotePeer/RemotePeer.js
@@ -0,0 +1,62 @@
+import { useRemoteAudio, useRemoteVideo } from "@huddle01/react/hooks";
+import React, { useEffect, useRef } from "react";
+
+
+const RemotePeer = ({ peerId }) => {
+    const { stream, state } = useRemoteVideo({ peerId });
+    const { stream: audioStream, state: audioState } = useRemoteAudio({ peerId });
+    const vidRef = useRef(null);
+    const audioRef = useRef(null);
+
+    useEffect(() => {
+        if (stream && vidRef.current && state === "playable") {
+            vidRef.current.srcObject = stream;
+
+            vidRef.current.onloadedmetadata = async () => {
+                try {
+                    console.log("here 2");
+                    vidRef.current?.play();
+                } catch (error) {
+                    console.error(error);
+                }
+            };
+
+            vidRef.current.onerror = () => {
+                console.error("videoCard() | Error is hapenning...");
+            };
+        }
+    }, [stream]);
+
+    useEffect(() => {
+        if (audioStream && audioRef.current && audioState === "playable") {
+            audioRef.current.srcObject = audioStream;
+
+            audioRef.current.onloadedmetadata = async () => {
+                try {
+                    console.log("here 2");
+                    audioRef.current?.play();
+                } catch (error) {
+                    console.error(error);
+                }
+            };
+
+            audioRef.current.onerror = () => {
+                console.error("videoCard() | Error is hapenning...");
+            };
+        }
+    }, [audioStream]);
+
+    return (
+        <div>
+            <video
+                ref={vidRef}
+                autoPlay
+                muted
+                className="border-2 rounded-xl border-white-400 aspect-video"
+            />
+            <audio ref={audioRef} autoPlay></audio>
+        </div>
+    );
+};
+
+export default React.memo(RemotePeer);
